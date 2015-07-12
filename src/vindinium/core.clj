88ebb6f5c437b7574f479ea264b0f2bef6983934
Verna Koskinen <darth.kipsu@gmail.com>
2015-07-12 20:29:51 +0300

    display unparsed gamefield when running

--- src/vindinium/core.clj
+++ src/vindinium/core.clj
@@ -53,10 +53,33 @@
       (println (str "[" status "] " body))
       (throw+))))
 
+(defn unparse-tile [tile]
+  (match (:tile tile)
+         :air "  "
+         :wall "##"
+         :tavern "[]"
+         :mine (if (:of tile) (str "$" (:of tile)) "$-")
+         :hero (str "@" (:id tile))))
+
+(defn print-board [board-size tiles row column]
+  (if (= row board-size)
+    (println "")
+    (do (if (zero? column) (println ""))
+        (print (unparse-tile (first tiles)))
+        (recur board-size
+               (rest tiles)
+               (if (= board-size (inc column)) (inc row) row)
+               (if (= board-size (inc column)) 0 (inc column))))))
+
+(defn print-board-from-input [input]
+  (let [board (:board (:game input))
+        board-size (:size board)
+        tiles (:tiles board)]
+    (print-board board-size tiles 0 0)))
 
 (defn step [from]
   (loop [input from]
-    (print ".")
+    (print-board-from-input input)
     (let [next (request (:playUrl input) {:dir (bot input)})]
       (if (:finished (:game next)) (println "") (recur next)))))
 
